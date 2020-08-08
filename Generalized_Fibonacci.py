7a7e2e951a19b7331dfc6a64f4f68085ed3dfa70
Andoni Vizcaya <68577856+andonivizcaya@users.noreply.github.com>
2020-08-07 23:03:38 -0400

    Complex_Fibonacci
    This function calculates and plots complex-valued Fibonacci numbers.

--- Generalized_Fibonacci.py
+++ Generalized_Fibonacci.py
@@ -1,18 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
-import cmath
+import matplotlib.cm as cm
+import mpmath as mp
 from sys import exit
-from numpy import pi, exp, real, imag, linspace
+from numpy import exp, real, imag, linspace, arange
 from mpl_toolkits.mplot3d import Axes3D
+#here you need to install mpmath with pip or conda
 
+# normalizing and applying color map to a given ndarray
+def sufacecolor(angpart):
+    cmap = cm.jet
+    norm = plt.Normalize(vmin=angpart.min(), vmax=angpart.max())
+    surfang = cmap(norm(angpart))
 
+    return surfang
+
+# calculates phi and psi global variables
 phi = ((1 + np.sqrt(5)) / 2)
 psi = (-1 / phi)
 
-# work in progress
+# here you can choose between some visualization options
 def MultiChoice():
-    print("Now you can choose between this options:")
+    print("Now you can choose between these options:")
     print("1\tThe plot of the positive real input (x>0; y=0)")
     print("2\tThe plot of the negative real input (x<0; y=0)")
     print("3\tThe plot of the positive imaginary input (x=0; y>0)")
@@ -40,14 +49,14 @@ def MultiChoice():
         print("That's not a valid option, try again.")
         return MultiChoice()
 
-
+# positive real valued complex plane of the Fibonacci function
 def real_pos():
     X = linspace(0.0, 7.0, 101, dtype=np.complex)
     kk = 0
     F_x = np.zeros(np.size(X), dtype=np.complex)
+    Psi = np.complex(psi)
 
     for i in X:
-        Psi = np.complex(psi)
         F_x[kk] = ((phi**(i)) - (Psi**(i))) / np.sqrt(5)
         kk = kk + 1
 
@@ -59,13 +68,14 @@ def real_pos():
 
     plt.show()
 
+# negative real valued complex plane of the Fibonacci function    
 def real_neg():
     X = linspace(-7.0, 0.0, 142, dtype=np.complex)
     kk = 0
     F_x = np.zeros(np.size(X), dtype=np.complex)
+    Psi = np.complex(psi)
 
     for i in X:
-        Psi = np.complex(psi)
         F_x[kk] = ((phi**(i)) - (Psi**(i))) / np.sqrt(5)
         kk = kk + 1
 
@@ -77,14 +87,14 @@ def real_neg():
 
     plt.show()
 
-
+# positive imaginary valued complex plane of the Fibonacci function
 def im_pos():
     X = 1j * linspace(0.0, 15.0, 151, dtype=np.complex)
     kk = 0
     F_x = np.zeros(np.size(X), dtype=np.complex)
+    Psi = np.complex(psi)
 
     for i in X:
-        Psi = np.complex(psi)
         F_x[kk] = ((phi**(i)) - (Psi**(i))) / np.sqrt(5)
         kk = kk + 1
 
@@ -96,14 +106,14 @@ def im_pos():
 
     plt.show()
 
-
+# negative imaginary valued complex plane of the Fibonacci function
 def im_neg():
     X = 1j * linspace(-15.0, 0.0, 151, dtype=np.complex)
     kk = 0
     F_x = np.zeros(np.size(X), dtype=np.complex)
+    Psi = np.complex(psi)
 
     for i in X:
-        Psi = np.complex(psi)
         F_x[kk] = ((phi**(i)) - (Psi**(i))) / np.sqrt(5)
         kk = kk + 1
 
@@ -115,16 +125,85 @@ def im_neg():
 
     plt.show()
 
-
+# real part of the complex valued Fibonacci function
 def re_comp():
-    print("i'm working, this one is hard")
-     
+    mp.dps = 5
+    Phi = ((1 + mp.sqrt(5)) / 2)
+    Psi = (-1 / phi)
 
+    f = lambda z: real((mp.power(Phi,z) - mp.power(Psi,z)) / mp.sqrt(5))
 
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    X = arange(-5, 5, 0.125)
+    Y = arange(-5, 5, 0.125)
+    X, Y = np.meshgrid(X, Y)
+    xn, yn = X.shape
+    W = X*0
+    for xk in range(xn):
+        for yk in range(yn):
+            try:
+                z = complex(X[xk,yk],Y[xk,yk])
+                w = float(f(z))
+                if w != w:
+                    raise ValueError
+                W[xk,yk] = w
+            except (ValueError, TypeError, ZeroDivisionError):
+                pass
+
+    SW = sufacecolor(W)
+    ax.plot_surface(X, Y, W, rstride=1, cstride=1, facecolors=SW)
+    ax.set_xlabel('Re')
+    ax.set_ylabel('im')
+    ax.set_zlabel('Re(F(z))')
+    plt.title('Real part of Complex-Valued Fibonacci')
+
+    M = cm.ScalarMappable(cmap=cm.jet)
+    M.set_array(SW)
+    plt.colorbar(M)
+
+    plt.show()  
+
+# imaginary part of the complex valued Fibonacci function
 def im_comp():
-    print("i'm working, this one is hard")
-    # this is hard
+    mp.dps = 5
+    Phi = ((1 + mp.sqrt(5)) / 2)
+    Psi = (-1 / phi)
 
+    f = lambda z: imag((mp.power(Phi,z) - mp.power(Psi,z)) / mp.sqrt(5))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    X = arange(-5, 5, 0.125)
+    Y = arange(-5, 5, 0.125)
+    X, Y = np.meshgrid(X, Y)
+    xn, yn = X.shape
+    W = X*0
+    for xk in range(xn):
+        for yk in range(yn):
+            try:
+                z = complex(X[xk,yk],Y[xk,yk])
+                w = float(f(z))
+                if w != w:
+                    raise ValueError
+                W[xk,yk] = w
+            except (ValueError, TypeError, ZeroDivisionError):
+                pass
+
+    SW = sufacecolor(W)
+    ax.plot_surface(X, Y, W, rstride=1, cstride=1, facecolors=SW)
+    ax.set_xlabel('Re')
+    ax.set_ylabel('im')
+    ax.set_zlabel('Re(F(z))')
+    plt.title('Imaginary part of Complex-Valued Fibonacci')
+
+    M = cm.ScalarMappable(cmap=cm.jet)
+    M.set_array(SW)
+    plt.colorbar(M)
+
+    plt.show()
+
+# Main function. It calls the other functions and calculates the corresponding Fibonacci numbeer.
 def gigafibonaci():
     print("""This program takes as an input any number
     (real or complex) and returns the corresponding fibonacci number.
